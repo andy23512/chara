6a008ae71ff5a7e2aaf48c38532f62b1031b3b01
Tangent Chang <andy23512@gmail.com>
2026-05-13 19:52:06 +0800

    feat: set locale text in ag-grid

--- src/app/pages/chords-page/chords-page.component.ts
+++ src/app/pages/chords-page/chords-page.component.ts
@@ -1,4 +1,9 @@
 import {
+  AG_GRID_LOCALE_EN,
+  AG_GRID_LOCALE_JP,
+  AG_GRID_LOCALE_TW,
+} from '@ag-grid-community/locale';
+import {
   ChangeDetectionStrategy,
   Component,
   computed,
@@ -28,9 +33,11 @@ import { lastValueFrom } from 'rxjs';
 import { AncestorsKeyLabelsRendererComponent } from 'src/app/components/ancestors-key-labels-renderer/ancestors-key-labels-renderer.component';
 import { ChordKeyLabelsRendererComponent } from 'src/app/components/chord-key-labels-renderer/chord-key-labels-renderer.component';
 import { ChordData } from 'src/app/models/chord.models';
+import { UiLanguage } from 'src/app/models/language-setting.models';
 import { OperatingSystemService } from 'src/app/services/operating-system.service';
 import { FlatChordTreeNodeStore } from 'src/app/stores/flat-chord-tree-node.store';
 import { KeyboardLayoutSettingStore } from 'src/app/stores/keyboard-layout-setting.store';
+import { LanguageSettingStore } from 'src/app/stores/language-setting.store';
 import {
   convertFlattenedChordTreeNodesToChordData,
   flattenChordTreeNodes,
@@ -75,8 +82,27 @@ export class ChordsPageComponent {
   public flatChordTreeNodes = this.flatChordTreeNodeStore.entities;
   public keyboardLayout = inject(KeyboardLayoutSettingStore).selectedEntity;
   public operatingSystem = inject(OperatingSystemService);
+  public languageSettingStore = inject(LanguageSettingStore);
   public fileInput =
     viewChild.required<ElementRef<HTMLInputElement>>('fileInput');
+
+  public localeText = computed(() => {
+    const uiLanguage = this.languageSettingStore.uiLanguage();
+    switch (uiLanguage) {
+      case UiLanguage.EN:
+        return { ...AG_GRID_LOCALE_EN, noRowsToShow: 'No Chords To Show' };
+      case UiLanguage.ZH_TW:
+        return { ...AG_GRID_LOCALE_TW, noRowsToShow: '沒有和弦可顯示' };
+      case UiLanguage.JP:
+        return {
+          ...AG_GRID_LOCALE_JP,
+          noRowsToShow: '表示する和音がありません',
+        };
+      default:
+        uiLanguage satisfies never;
+        return {};
+    }
+  });
 
   public chordDataList = computed(() => {
     const keyboardLayout = this.keyboardLayout();
