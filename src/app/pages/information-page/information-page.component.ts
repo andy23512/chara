0213990f4921942af8d66d3022df62eb492e6879
Tangent Chang <andy23512@gmail.com>
2026-05-13 21:49:28 +0800

    fix: fix deploy error

--- src/app/pages/information-page/information-page.component.ts
+++ src/app/pages/information-page/information-page.component.ts
@@ -1,20 +1,9 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
-import { IconGuardPipe } from 'src/app/pipes/icon-guard.pipe';
-import { RealTitleCasePipe } from 'src/app/pipes/real-title-case.pipe';
 
 @Component({
   selector: 'app-information-page',
   standalone: true,
-  imports: [
-    RouterLink,
-    MatIcon,
-    IconGuardPipe,
-    TranslatePipe,
-    RealTitleCasePipe,
-  ],
+  imports: [],
   templateUrl: './information-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
