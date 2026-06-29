cd8f373703c3b0c6fd50d94d77409ac9ed480374
Tangent Chang <andy23512@gmail.com>
2026-06-29 22:40:11 +0800

    fix: fix build problem

--- src/app/app.worker.ts
+++
@@ -1,7 +0,0 @@
-/// <reference lib="webworker" />
-import { WebWorkerMLCEngineHandler } from '@mlc-ai/web-llm';
-
-const handler = new WebWorkerMLCEngineHandler();
-addEventListener('message', (msg: MessageEvent) => {
-  handler.onmessage(msg);
-});
